83cd4cf8ac57fb968939b1d97524e5ec608da4bd
Olivier Lemarchand <olemarchand@indeed.com>
2019-01-18 14:24:01 +0900

    added test failing on bazel_1 that should pass from bazel_2

--- BUILD
+++ BUILD
@@ -11,3 +11,17 @@ sh_test(
     ],
 )
 
+sh_test(
+    name = "toto_tst_from_bazel_2",
+    srcs = [
+        "toto_test_from_bazel_2.sh",
+    ],
+    data = [
+        "toto.txt",
+    ],
+    deps = [
+        "@bazel_tools//tools/bash/runfiles",
+    ],
+)
+
+
---
+++ toto_test_from_bazel_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CURRENT_DIR="$(pwd)"
+echo ${CURRENT_DIR}
+
+source "external/bazel_1/toto.txt"
+
+function test_toto_content {
+
+  if [ $TOTO != "gah" ]; then
+    # This would be a failure case.
+    echo expected TOTO to be toto1 but was $TOTO
+    exit 1
+  fi
+}
+
+test_toto_content
